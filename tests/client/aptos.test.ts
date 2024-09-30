d6f1234f4044aed951519794abeeb15b1be30f58
v1xingyue <qixingyue@gmail.com>
2024-09-30 17:38:17 +0800

    basic load test

--- tests/client/aptos.test.ts
+++ tests/client/aptos.test.ts
@@ -1,43 +1,62 @@
-// import assert from "assert";
-// import { Stream } from "../../src";
-// import { Network } from "@aptos-labs/ts-sdk";
+import { Network } from "@aptos-labs/ts-sdk";
+import { Stream, aptos } from "../../src";
+import { test_private_key } from "../config";
+import assert from "assert";
 
-// describe("new stream object", () => {
-//   it("load aptos client tests", async () => {
-//     const stream = new Stream(Network.MAINNET);
-//     const aptos = stream.getAptosClient();
-//     const info = await aptos.getLedgerInfo();
-//     assert(info.chain_id === 1, "chain id should be 1");
-//     assert(
-//       info.oldest_ledger_version === "0",
-//       "oldest ledger version should be 0"
-//     );
-//     assert(
-//       parseInt(info.ledger_version) > 1690050882,
-//       "ledger version should be greater than 1690050882"
-//     );
-//     assert(
-//       parseInt(info.oldest_block_height) == 0,
-//       "oldest block height should be 0"
-//     );
-//     assert(
-//       parseInt(info.block_height) > 224630080,
-//       "block height should be greater than 224630080"
-//     );
-//   });
+describe("new stream object", () => {
+  const pair = new aptos.Ed25519PrivateKey(test_private_key);
+  const account = aptos.Account.fromPrivateKey({
+    privateKey: pair,
+  });
+  console.log("test account address: ", account.accountAddress.toString());
 
-//   it("change aptos network", async () => {
-//     const test_stream = new Stream(Network.TESTNET);
-//     const test_info = await test_stream.getAptosClient().getLedgerInfo();
-//     assert(test_info.chain_id === 2, "chain id should be 2");
-//   });
+  it("generate account", () => {
+    const pair = aptos.Account.generate();
+    console.log("new private key : ", pair.privateKey.toString());
+    const account = aptos.Account.fromPrivateKey({
+      privateKey: pair.privateKey,
+    });
+    console.log("new address : ", account.accountAddress.toString());
+  });
 
-//   it("use custom network", async () => {
-//     const custom_stream = new Stream(
-//       Network.CUSTOM,
-//       "https://rpc.ankr.com/premium-http/aptos/e7e60392a39c22ec1644a9a9f9dd6f239705887a109e0bbe645925c1a03bdac1/v1"
-//     );
-//     const custom_info = await custom_stream.getAptosClient().getLedgerInfo();
-//     assert(custom_info.chain_id === 1, "chain id should be 1");
-//   });
-// });
+  it("load aptos client tests", async () => {
+    console.log("mainnet client test ...");
+    const stream = new Stream(account.accountAddress, Network.MAINNET);
+    const aptos = stream.getAptosClient();
+    const info = await aptos.getLedgerInfo();
+    assert(info.chain_id === 1, "chain id should be 1");
+    assert(
+      info.oldest_ledger_version === "0",
+      "oldest ledger version should be 0"
+    );
+    assert(
+      parseInt(info.ledger_version) > 1690050882,
+      "ledger version should be greater than 1690050882"
+    );
+    assert(
+      parseInt(info.oldest_block_height) == 0,
+      "oldest block height should be 0"
+    );
+    assert(
+      parseInt(info.block_height) > 224630080,
+      "block height should be greater than 224630080"
+    );
+
+    console.log("testnet client test ...");
+
+    const test_stream = new Stream(account.accountAddress, Network.TESTNET);
+    const test_aptos = test_stream.getAptosClient();
+    const test_info = await test_aptos.getLedgerInfo();
+    assert(test_info.chain_id === 2, "test chain id should be 1");
+  });
+
+  it("use custom network rpc", async () => {
+    const custom_stream = new Stream(
+      account.accountAddress,
+      Network.CUSTOM,
+      "https://rpc.ankr.com/premium-http/aptos/e7e60392a39c22ec1644a9a9f9dd6f239705887a109e0bbe645925c1a03bdac1/v1"
+    );
+    const custom_info = await custom_stream.getAptosClient().getLedgerInfo();
+    assert(custom_info.chain_id === 1, "chain id should be 1");
+  });
+});
---
+++ tests/config.ts
@@ -0,0 +1,2 @@
+export const test_private_key =
+  "0x9628129d5420b0c7f67f089be18dc01011d4382ff604929a97b5886f292fa797";
