25e2538f152c2c8b0de2163d42f9304842c1ec19
v1xingyue <qixingyue@gmail.com>
2024-10-08 18:18:18 +0800

    clean examples

--- examples/stream-cli/test/example/example.test.ts
+++
@@ -1,9 +0,0 @@
-
-import assert from "assert";
-
-describe("add function", function () {
-  it("should return the sum of two numbers", function () {
-    assert.strictEqual(1 + 2, 3);
-  });
-}); 
-
